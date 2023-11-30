d657866b719ef289c14227d8766f30a8346e9c0f
infoananthk <ananth.kollipara@oracle.com>
2023-11-30 12:28:26 +0530

    template html pages added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,22 @@
+import pytest
 
-def test_generic():
+def test_generic() :
     a = 30
     b = 40
-    assert a==b
+    assert a!=b
+
+class NotInRange(Exception) :
+    def __init__(self, message="value not in range - oracle india") :
+        self.message = message
+        super().__init__(self.message)
+
+def test_generic1() :
+    a = 16
+    if a not in range(10, 20) :
+        raise NotInRange
+    
+def test_generic2() :
+    a = 5
+    with pytest.raises(NotInRange) :
+        if a not in range(10, 20) :
+            raise NotInRange
